48763b76e10ead189ff18017c2832b6cc8ef451a
spancavil <soancavil@gmail.com>
2023-05-21 13:20:47 -0300

    Added change

--- After 19 Mayo - Hasta clase 11/Relationships/Relationships/src/main/java/com/coderhouse/Relationships/model/BookRepository.java
+++ After 19 Mayo - Hasta clase 11/Relationships/Relationships/src/main/java/com/coderhouse/Relationships/model/BookRepository.java
@@ -3,5 +3,7 @@ package com.coderhouse.Relationships.model;
 import com.coderhouse.Relationships.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
